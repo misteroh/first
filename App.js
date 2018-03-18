4519b658c7dd4f907bea33edf77d167a4c2f983b
Andrew Oh <me@andrewoh.co>
2018-03-18 03:43:01 -0700

    init2

--- App.js
+++ App.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  CameraRoll,
+  Button,
+  ScrollView,
+} from 'react-native';
 
 export default class App extends React.Component {
+  _handleButtonPress = () => {
+    CameraRoll.getPhotos({
+        first: 20,
+        assetType: 'Photos',
+      })
+      .then(r => {
+        this.setState({ photos: r.edges });
+      })
+      .catch((err) => {
+         //Error Loading Images
+      });
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <View>
+        <Button title="Load Images" onPress={this._handleButtonPress} />
+        <ScrollView>
+          {this.state.photos.map((p, i) => {
+          return (
+            <Image
+              key={i}
+              style={{
+                width: 300,
+                height: 100,
+              }}
+              source={{ uri: p.node.image.uri }}
+            />
+          );
+        })}
+        </ScrollView>
+    </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
